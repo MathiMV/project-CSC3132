d36230f1e75b4a5209dcaaed912e5c5c89a84a03
MathiMV <139645735+MathiMV@users.noreply.github.com>
2024-12-01 07:47:26 +0000

    dbconfig file created

---
+++ pricatical/date20.11/dbconfig.php
@@ -0,0 +1,21 @@
+<?php
+define('SERVERNAME', 'localhost');
+define('USERNAME', 'root');
+define('PASSWORD', '');
+define('DBNAME', 'studentdb');
+try {
+	//connect with database
+	$connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
+	if (!$connect) {
+		die("connection failed".mysqli_connect_error());
+	} 
+	else {
+		echo "Connected successfully";
+	}
+} 
+catch (Exception $e) {
+	die($e->getMessage());
+}
+
+
+?>
